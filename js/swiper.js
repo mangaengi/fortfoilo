03b6467b0999e0a86d772f78bb9f4e42ba2545f5
mangaengi <cdr9898@naver.com>
2024-06-03 16:25:21 +0900

    프로젝트에 문구 추가

--- js/swiper.js
+++ js/swiper.js
@@ -1,37 +1,9 @@
-/* var swiper = new Swiper( '.project_Swiper', {
-        autoplay:5000,
-        speed : 5000,
-        effect: 'coverflow',
-        loop: true,
-        centeredSlides: true,
-        slidesPerView: 'auto',
-        coverflow: {
-            rotate: 0,
-            stretch: 100,
-            depth: 400,
-            modifier: 1.5,
-            slideShadows : true,
 
-        }
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        }
-        
-    } ); */
-
-/*     var swiper = new Swiper(".project_Swiper", {
-        loop: true,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    }); */
     var swiper = new Swiper(".project_Swiper", {
         spaceBetween: 30,
         loop: true,
         pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
+            el: ".swiper-pagination",
+            clickable: true,
         },
-      });
+    });
